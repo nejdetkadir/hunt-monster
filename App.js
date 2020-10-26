b7bb4b253871f92f73f2822ec3fc54b4b693a902
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-26 21:24:06 +0300

    Optimize functions and statements

--- App.js
+++ App.js
@@ -1,69 +1,93 @@
 new Vue({
   el: "#app",
   data: {
-    playerHeal: 100,
-    monsterHeal: 100,
+    playerHealth: 100,
+    monsterHealth: 100,
     gameIsStarted: false,
-    logs: []
+    attackMultiple: 15,
+    specialAttackMultiple: 25,
+    monsterAttackMultiple: 22,
+    firstAidMultiple: 18,
+    logs: [],
+    log_text: {
+      attack: "PLAYER ATTACK ",
+      monsterAttack: "MONSTER ATTACK ",
+      specialAttack: "SPECIAL PLAYER ATTACK ",
+      firstAid: "FIRST AID ",
+      giveUp: "PLAYER GIVE UP!",
+      lose: "You lost this round. Do you want to try again?",
+      win: "You win this round. Do you want to game again?"
+    }
   },
   methods: {
     startGame: function() {
       this.gameIsStarted = true;
     },
     attack: function() {
-      const point = Math.ceil(Math.random() * 10);
-      this.monsterHeal -= point;
-      this.addLog({turn: "player-turn", text: `PLAYER ATTACK (${point})`});
+      const point = Math.ceil(Math.random() * this.attackMultiple);
+      this.monsterHealth -= point;
+      this.addLog({turn: "player-turn", text: this.log_text.attack + point});
       this.monsterAttack();
     },
     monsterAttack: function() {
-      const point = Math.ceil(Math.random() * 10);
-      this.playerHeal -= point;
-      this.addLog({turn: "monster-turn", text: `MONSTER ATTACK (${point})`});
+      const point = Math.ceil(Math.random() * this.monsterAttackMultiple);
+      this.playerHealth -= point;
+      this.addLog({turn: "monster-turn", text: this.log_text.monsterAttack + point});
     },
     specialAttack: function() {
-      const point = Math.ceil(Math.random() * 25);
-      this.monsterHeal -= point;
-      this.addLog({turn: "player-turn", text: `SPECIAL PLAYER ATTACK (${point})`});
+      const point = Math.ceil(Math.random() * this.specialAttackMultiple);
+      this.monsterHealth -= point;
+      this.addLog({turn: "player-turn", text: this.log_text.specialAttack + point});
       this.monsterAttack();
     },
     firstAid: function() {
-      const point = Math.ceil(Math.random() * 20);
-      this.playerHeal += point;
-      this.addLog({turn: "player-turn", text: `FIRST AID (${point})`});
+      const point = Math.ceil(Math.random() * this.firstAidMultiple);
+      this.playerHealth += point;
+      this.addLog({turn: "player-turn", text: this.log_text.firstAid + point});
       this.monsterAttack();
     },
     giveUp: function() {
-      this.playerHeal = 0;
-      this.addLog({turn: "player-turn", text: 'PLAYER GIVE UP!'});
+      this.playerHealth = 0;
+      this.addLog({turn: "player-turn", text: this.log_text.giveUp});
     },
     addLog: function(log) {
       this.logs.push(log);
+    },
+    gameFinish: function(type) {
+      if(confirm( type === false ? this.log_text.lose : this.log_text.win)) {
+        this.playerHealth = 100;
+        this.monsterHealth = 100;
+        this.logs = [];
+      }
     }
   },
   watch: {
-    playerHeal: function(val) {
+    playerHealth: function(val) {
       if (val <= 0) {
-        this.playerHeal = 0;
-        if(confirm("You lost this round. Do you want to try again?")) {
-          this.playerHeal = 100;
-          this.monsterHeal = 100;
-          this.logs = [];
-        }
+        this.playerHealth = 0;
+        this.gameFinish(false);
       } else if (val >= 100) {
-        this.playerHeal = 100;
+        this.playerHealth = 100;
       }
     },
-    monsterHeal: function(val) {
+    monsterHealth: function(val) {
       if (val <= 0) {
-        this.monsterHeal = 0;
-        if(confirm("You win this round. Do you want to game again?")) {
-          this.playerHeal = 100;
-          this.monsterHeal = 100;
-          this.logs = [];
-        }
+        this.monsterHealth = 0;
+        this.gameFinish(true);
       } else if (val >= 100) {
-        this.monsterHeal = 100;
+        this.monsterHealth = 100;
+      }
+    }
+  },
+  computed: {
+    playerProgress: function() {
+      return {
+      width: this.playerHealth + '%'
+      }
+    },
+    monsterProgress: function() {
+      return {
+        width: this.monsterHealth + '%'
       }
     }
   }
