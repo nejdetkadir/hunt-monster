ff54b09cfccded725a35e46272f71b38594e4bb2
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-26 20:13:58 +0300

    Create variables and functions of environment

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 new Vue({
   el: "#app",
   data: {
-
+    playerHeal: 100,
+    monsterHeal: 100,
+    gameIsStarted: false
   },
   methods: {
+    startGame: function () {
+      this.gameIsStarted = true;
+    },
+    attack: function () {
 
+    },
+    specialAttack: function () {
+
+    },
+    firstAid: function () {
+
+    },
+    giveUp: function () {
+
+    }
   }
 });
